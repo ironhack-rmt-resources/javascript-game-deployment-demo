8c59ccda647d505e4ff29e35009769a400beed2f
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2023-10-31 15:55:08 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+
+const player = new Player();
+const obstaclesArr = []; // will store instances of the class Obstacle
+
+
+// create obstacles
+setInterval(() => {
+    const newObstacle = new Obstacle();
+    obstaclesArr.push(newObstacle);
+}, 3000);
+
+
+
+// update obstacles
+setInterval(() => {
+    obstaclesArr.forEach( (obstacleInstance) => {
+
+        // move
+        obstacleInstance.moveDown();
+
+        // remove obstacles if outside
+        if(obstacleInstance.positionY < 0 - obstacleInstance.height){
+            obstacleInstance.obstacleElm.remove(); // remove dom element
+            obstaclesArr.shift(); // remove from the array
+        }
+
+        // detect collision
+        if (
+            player.positionX < obstacleInstance.positionX + obstacleInstance.width &&
+            player.positionX + player.width > obstacleInstance.positionX &&
+            player.positionY < obstacleInstance.positionY + obstacleInstance.height &&
+            player.positionY + player.height > obstacleInstance.positionY
+        ) {
+            // Collision detected!
+            console.log("game over!");
+            // location.href = "./gameover.html";
+        }
+    });
+}, 30);
+
+
+// attach event listeners
+document.addEventListener("keydown", (e) => {
+    switch (e.code) {
+        case "ArrowLeft":
+            player.moveLeft();
+            break;
+        case "ArrowRight":
+            player.moveRight();
+            break;
+    }
+});
